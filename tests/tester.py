86bc6c1ac1e9cf7d6e1fe257a24756279ef45f8a
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2022-07-02 18:09:23 +0200

    update tester

---
+++ tests/tester.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 28 11:47:42 2022
+
+@author: alessandrosebastianelli
+"""
+        
+from opensv.io.reader import load
+from opensv.post.composite import rgb_composite
+from opensv.pre.normalizer import max_scaler
+from opensv.plot.plot import plot, bands_plot
+
+
+
+img, meta, bounds = load('tests/data/S2-lat_45_85299971127813_lon_10_852932810361423-2019-06-21.tif')
+
+#load('data/S2-lat_45_85299971127813_lon_10_852932810361423-2019-06-21.tif')
+
+#load('data/S2-lat_45_85299971127813_lon_10_852932810361423-2019-06-13_2.tif')
+img = max_scaler(img, mmax = 10000)
+rgb = 3*rgb_composite(img, rgb=[3,2,1])
+
+
+bands_plot(img, hist=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
