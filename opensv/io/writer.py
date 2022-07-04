0145118bd42fcc5b68d8b8e6f03e881fef66062b
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2022-07-04 19:28:28 +0200

    Update writer.py

--- opensv/io/writer.py
+++ opensv/io/writer.py
@@ -7,5 +7,17 @@ Created on Sun Jun 26 17:49:04 2022
 """
 
 
-def save(): pass
-    
+def save(image, path):
+
+    RASTERIO_EXTENSIONS   = ['.tif', '.tiff']
+    MATPLOTLIB_EXTENSIONS = ['.png', '.jpg', 'jpeg']
+
+    if any(frmt in path for frmt in RASTERIO_EXTENSIONS):
+        # To be implemented, save as tiff
+        pass
+
+    elif any(frmt in path for frmt in MATPLOTLIB_EXTENSIONS):
+        plt.imsave(path, image)
+
+    else:
+        print('[!] File can not be saved, format not supported!')
