ec683ab1c36bfa1c22e2953f59574270bc4c37bc
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2022-07-04 20:01:34 +0200

    Update writer.py

--- opensv/io/writer.py
+++ opensv/io/writer.py
@@ -8,6 +8,14 @@ Created on Sun Jun 26 17:49:04 2022
 
 
 def save(image, path):
+    '''
+
+        Save an image and its metadata given its path
+
+        Inputs:
+            - image: the image to be saved
+            - path: position of the image
+    '''
 
     RASTERIO_EXTENSIONS   = ['.tif', '.tiff']
     MATPLOTLIB_EXTENSIONS = ['.png', '.jpg', 'jpeg']
