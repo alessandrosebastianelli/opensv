786535cedc8ea81ce7fc19ae86fa5ffd51bc1fb8
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2022-07-05 21:23:31 +0200

    update cube

--- opensv/plot/cube.py
+++ opensv/plot/cube.py
@@ -6,35 +6,33 @@ Created on Tue Jul 5 14:01:04 2022
 @author: alessandrosebastianelli
 """
 
-import matplotlib.pyplot as plt
-import numpy as np
-
-import matplotlib.pyplot as plt
 from mayavi import mlab
 
-
-def plot3d(image, animate):
+def plot3d(image, animate = False):
     '''
         3D-Plot of a multibands image.
-        
+
         Input:
             - image: a WxHxB image, with height H, width W and B bands
             - animate: activate the animation mode
 
     '''
 
-    fig = mlab.figure(figure='Plote 3D', bgcolor=(1, 1, 1), fgcolor=(0, 0, 0))
+    fig = mlab.figure(figure='Plot 3D', bgcolor=(1, 1, 1), fgcolor=(0, 0, 0), size=(600, 600))
 
     v = mlab.volume_slice(image, slice_index=0, plane_orientation='z_axes', figure=fig)  # depth slice
 
     if animate:
         @mlab.animate
         def anim():
-            for i in range(image.shape[-1]):
-                v.mlab_source.scalars = image[:, :, i]
+            ct = 0
+            while True:
+                if ct >= image.shape[-1]:
+                    ct = 0
+                v.mlab_source.scalars = image[:, :, ct]
+                ct += 1
+
                 yield
         anim()
 
     mlab.show()
-
-
