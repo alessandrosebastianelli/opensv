d575edf716c470fec28ac4845d3f3cddacf1a253
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2022-07-06 15:20:48 +0200

    printer

--- opensv/utils/printer.py
+++ opensv/utils/printer.py
@@ -6,10 +6,45 @@ Created on Sat Jun 25 20:19:40 2022
 @author: alessandrosebastianelli
 """
 
-def dict_disp(dict):
+import numpy as np
 
+
+def dict_disp(dict):
+    '''
+        Print a dictionary in a more readable way
+
+        Input:
+            - dict: python dictionary to print
+
+    '''
     print('//----------- Dictionary -----------//')
 
     for key, value in dict.items():
         print('\t [*] {} : {}'.format(key, value))
-        
+
+
+def print_stats(image, perband=False):
+    '''
+        Print some statistics for the input image
+
+        Inputs:
+            - image: a WxHxB image, with width W, height H and B bands
+            - perband (optional): if true the function will print the statistics for each band separately
+    '''
+
+    print('//----------- Image Statistics -----------//')
+    if perband:
+
+        for i in range(image.shape[-1]):
+            print('\t Band #{}'.format(i))
+            print('\t\t Max                {0:.5f}'.format(np.max(image[:, :, i])))
+            print('\t\t Min                {0:.5f}'.format(np.min(image[:, :, i])))
+            print('\t\t Mean               {0:.5f}'.format(np.mean(image[:, :, i])))
+            print('\t\t Standard Deviation {0:.5f}'.format(np.std(image[:, :, i])))
+            print('\t\t Median             {0:.5f}'.format(np.median(image[:, :, i])))
+    else:
+        print('\t Max                {0:.5f}'.format(np.max(image)))
+        print('\t Min                {0:.5f}'.format(np.min(image)))
+        print('\t Mean               {0:.5f}'.format(np.mean(image)))
+        print('\t Standard Deviation {0:.5f}'.format(np.std(image)))
+        print('\t Median             {0:.5f}'.format(np.median(image)))
--- tests/tester.py
+++ tests/tester.py
@@ -16,6 +16,7 @@ from opensv.io.reader import load
 from opensv.post.composite import rgb_composite
 from opensv.pre.normalizer import max_scaler
 from opensv.plot.cube import cube_plot
+from opensv.utils.printer import print_stats
 
 
 img, meta, bounds = load('tests/data/S2-lat_45_85299971127813_lon_10_852932810361423-2019-06-21.tif')
@@ -24,8 +25,9 @@ rgb = 3*rgb_composite(img, rgb=[3, 2, 1])
 
 expanded = np.repeat(img[:,:,:-3], 20, axis=-1)
 
-
-cube_plot(expanded)
+print_stats(img, perband=False)
+print_stats(img, perband=True)
+#cube_plot(expanded)
 
 
 
