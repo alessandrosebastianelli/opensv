9d5329b37721af0c9279e3c3e2fe49e7e0050f39
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2022-07-07 11:08:28 +0200

    Update data_reduction.py

--- opensv/ai/data_reduction.py
+++ opensv/ai/data_reduction.py
@@ -9,8 +9,9 @@ Created on Thu Jul 7 10:56:04 2022
 from sklearn.decomposition import PCA
 
 
-def image_PCA(image, n_components=3):
+def image_PCA(image, n_components):
     '''
+
         Reduce the image dimensionality along the frequency axis
 
         Inputs:
@@ -25,3 +26,21 @@ def image_PCA(image, n_components=3):
     reduced = reduced.reshape((image.shape[0], image.shape[1], n_components))
 
     return reduced
+
+
+def image_stack_PCA(images, n_components):
+    '''
+
+        !!!To be implemented!!!
+
+        Reduce a temporal stack of images along the temporal axis
+
+        Inputs:
+            - images: a TxWxHxB stack of images, with T temporal length, width W, height H and B bands
+            - n_components: number of bands of the compressed image
+        Output:
+            - reduced: the n_componentsxWxHxB compressed image, n_components temporal length, with width W, height H and B bands
+
+    '''
+
+    pass
