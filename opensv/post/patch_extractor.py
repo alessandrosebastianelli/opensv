1c38263f3fe3795e66a10ee1c8fb5e64329c425a
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2022-07-07 17:15:12 +0200

    Update patch_extractor.py

--- opensv/post/patch_extractor.py
+++ opensv/post/patch_extractor.py
@@ -37,3 +37,25 @@ def patch_extractor(image, shape = (64, 64), stride = (1,1)):
             ct+=1
 
     return patches
+
+def patch_iterator(image, shape = (64, 64), stride = (1,1)):
+    '''
+
+        (Iterator) Extracts patches of 'shape' from 'image' with 'stride'
+
+        Inputs:
+            - image: a WxHxB image, with width W, height H and B bands
+            - shape (optional): shape of the patches
+            - stride (optional): stride of the moving window
+        Output:
+            - patch: a shape(0)xshape(1)xB patch of width shape(0), height shape(1) and B bands
+    '''
+
+    width, height, channels = image.shape
+
+    w_out = 1 + ((width - shape[0]) // stride[0])
+    h_out = 1 + ((height - shape[1]) // stride[1])
+
+    for j in range(0, height - shape[1], stride[1]):
+        for i in range(0, width - shape[0], stride[0]):
+            yield image[j:j+shape[1], i:i+shape[0], :]
