e3dc6179f894318fa89e869bdc874d09ce6c157c
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2022-07-07 10:33:14 +0200

    new module

--- opensv/ai/clustering.py
+++ opensv/ai/clustering.py
@@ -21,13 +21,11 @@ def pixel_clustering(img, n_clusters = 3):
             - cliusted: clustered pixels
     '''
     
-    # For clustering the image using k-means, we first need to convert it into a 2-dimensional array
-    image_2D = img.reshape(img.shape[0]*img.shape[1], img.shape[2])
 
+    image_2D = img.reshape(img.shape[0]*img.shape[1], img.shape[2])
     kmeans = KMeans(n_clusters = n_clusters, random_state = 0).fit(image_2D)
-    #clustered = kmeans.cluster_centers_[kmeans.labels_]
-    #clustered_3D = clustered.reshape(img.shape[0], img.shape[1], img.shape[2])
-    
     cliusted = np.array(kmeans.labels_).reshape(img.shape[0], img.shape[1])
     
     return cliusted
+
+def conv_clustering(): pass
