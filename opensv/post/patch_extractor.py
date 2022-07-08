340821a37b4fdf170e227afadc897072d7331371
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2022-07-08 09:45:43 +0200

    Update patch_extractor.py

--- opensv/post/patch_extractor.py
+++ opensv/post/patch_extractor.py
@@ -9,7 +9,7 @@ Created on Thu Jul 7 16:16:42 2022
 import numpy as np
 
 
-def patch_extractor(image, shape = (64, 64), stride = (1,1)):
+def patch_extractor(image, shape=(64, 64), stride=(1, 1)):
     '''
 
         Extracts patches of 'shape' from 'image' with 'stride'
@@ -27,18 +27,19 @@ def patch_extractor(image, shape = (64, 64), stride = (1,1)):
     w_out = 1 + ((width - shape[0]) // stride[0])
     h_out = 1 + ((height - shape[1]) // stride[1])
 
-    patches = np.zeros((w_out*h_out, shape[0], shape[1], channels))
+    patches = np.zeros((w_out * h_out, shape[0], shape[1], channels))
 
     ct = 0
     for j in range(0, height - shape[1], stride[1]):
         for i in range(0, width - shape[0], stride[0]):
-            patches[ct, :, :, :] = image[j:j+shape[1], i:i+shape[0], :]
+            patches[ct, :, :, :] = image[j:j + shape[1], i:i + shape[0], :]
 
-            ct+=1
+            ct += 1
 
     return patches
 
-def patch_iterator(image, shape = (64, 64), stride = (1,1)):
+
+def patch_iterator(image, shape=(64, 64), stride=(1, 1)):
     '''
 
         (Iterator) Extracts patches of 'shape' from 'image' with 'stride'
@@ -58,4 +59,4 @@ def patch_iterator(image, shape = (64, 64), stride = (1,1)):
 
     for j in range(0, height - shape[1], stride[1]):
         for i in range(0, width - shape[0], stride[0]):
-            yield image[j:j+shape[1], i:i+shape[0], :]
+            yield image[j:j + shape[1], i:i + shape[0], :]
