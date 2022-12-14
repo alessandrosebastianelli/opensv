d82572a7fccc06a4e6e52f216c8a372801aa51ac
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2022-12-14 17:01:25 +0100

    writer - write with rasterio

--- opensv/io/writer.py
+++ opensv/io/writer.py
@@ -7,7 +7,7 @@ Created on Sun Jun 26 17:49:04 2022
 """
 
 
-def save(image, path):
+def save(image, path, srcimage=None):
     '''
 
         Save an image and its metadata given its path
@@ -15,14 +15,28 @@ def save(image, path):
         Inputs:
             - image: the image to be saved
             - path: position of the image
+            - srcimage (optional): source image, used to copy metdata and CRS, must be a rasterio object
     '''
 
     RASTERIO_EXTENSIONS   = ['.tif', '.tiff']
     MATPLOTLIB_EXTENSIONS = ['.png', '.jpg', 'jpeg']
 
     if any(frmt in path for frmt in RASTERIO_EXTENSIONS):
-        # To be implemented, save as tif
-        pass
+
+        if srcimage!=None:
+            meta = srcimage.meta.copy()
+            meta.update({'driver':'GTiff',
+                            'width':image.shape[0],
+                            'height':image.shape[1],
+                            'count':image.shape[2],
+                            'dtype':'float64',
+                            'crs':srcimage.crs, 
+                            'transform':srcimage.transform,
+                            'nodata':0})
+
+        with rasterio.open(fp=path, mode='w',**out_meta) as dst:
+            for count in range(image.shape[2]):
+                dst.write(image[:,:,count], count)
 
     elif any(frmt in path for frmt in MATPLOTLIB_EXTENSIONS):
         plt.imsave(path, image)
