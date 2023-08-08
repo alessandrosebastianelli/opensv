53f9c8f36beefe549c35c3ea15cdafca5555fb24
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2023-08-08 14:06:13 +0200

    better doc

--- pyosv/plot/plot.py
+++ pyosv/plot/plot.py
@@ -1,15 +1,58 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot(img, hist = False):
+def plot(img : np.ndarray, hist : bool = False) -> None:
     '''
-        Plot an image and its histogram
+        Plot a satellite image and its histogram.
+
+        Only gray-scale or RGB-like are accepted.
+
+        Parameters:
+        ----------
+            - img : np.ndarray
+                a WxHxB image, with width W, height H and B bands (B can be 3 or 1)
+            - hist : bool
+                if True display also the histogram (dafault : False)
         
-        Inputs:
-            - img: a WxHxB image, with width W, height H and B bands (B can be 3 or 1)
-            - hist (optional): if True display also the histogram
+        Returns:
+        --------
+        Nothing, it will display the image
+
+        Usage:
+        ------
+
+        ```python
+        import numpy as np
+
+        img         = np.array(  
+            [[
+                [0.1, 0.2, 0.3],  
+                [0.4, 0.5, 0.6],  
+                [0.7, 0.8, 0.9]
+             ]
+            ]  
+        ) 
+
+        # Adding fake axis
+        img = img[:,:,None]
+
+        plot(img, hist=True)
+
+        ```
+
+        Output:
+        -------
+        Nothing, it will display the image
     '''
     
+
+    if len(img.shape) != 3:
+        raise Exception('Error: lenght of image shape must be 3 - (space, space, channels)')
+
+    if ((img.shape[-1] != 1) and (img.shape[-1] != 3)):
+        raise Exception("Error: numer of channels admitted are 1 or 3")
+
+
     ncols = 1
     if hist: ncols = 2
     
@@ -18,7 +61,6 @@ def plot(img, hist = False):
     if hist:
         axes[0].imshow(img)
         axes[0].set_title('Image')
-        
         for b in range(img.shape[-1]):
             axes[1].hist(img[:,:,b].flatten(), 200, label='Band {}'.format(b))
         axes[1].legend()
@@ -31,39 +73,73 @@ def plot(img, hist = False):
     plt.show()
 
 
-def bands_plot(img, hist = False):
+def bands_plot(img : np.ndarray, hist : bool = False):
     '''
-        Plot image bands and relative histograms
-        
-        Inputs:
-            - img: a WxHxB image, with width W, height H and B bands
-            - hist (optional): if True display also the histogram
-    '''    
-    
-    bands = img.shape[-1]
-    nrows = int(np.sqrt(bands))
-    
+        Plot a satellite image bands and relative histograms.
 
-    ncols = nrows
-    if hist: ncols = 2*nrows
+        Parameters:
+        ----------
+            - img : np.ndarray
+                a WxHxB image, with width W, height H and B bands
+            - hist : bool
+                if True display also the histograms (dafault : False)
+        
+        Returns:
+        --------
+        Nothing, it will display the image
+
+        Usage:
+        ------
+
+        ```python
+        import numpy as np
+
+        img         = np.array(  
+            [[
+                [0.1, 0.2, 0.3],  
+                [0.4, 0.5, 0.6],  
+                [0.7, 0.8, 0.9]
+             ],
+             [
+                [1.1, 1.2, 1.3],  
+                [1.4, 1.5, 1.6],  
+                [1.7, 1.8, 1.9]
+             ]
+            ]  
+        ) 
+
+        # Adding fake axis
+        img = np.moveaxis(img, 0, -1)
+
+        bands_plot(img, hist=True)
+
+        ```
+
+        Output:
+        -------
+        Nothing, it will display the image
+    '''
+    
+    if len(img.shape) != 3:
+        raise Exception('Error: lenght of image shape must be 3 - (space, space, channels)')
+    
+    ncols = img.shape[-1]
+    nrows = 1
+    if hist: nrows = 2
     
     fig, axes = plt.subplots(nrows = nrows, ncols = ncols, figsize = (4*ncols, 4*nrows))
-    
-    if hist: ncols = nrows
-    
-    ct = 0
-    for r in range(nrows):
-        for c in range(ncols):
-            if hist:
-                axes[r,2*c].imshow(img[:,:,ct])
-                axes[r,2*c].set_title('Bands {}'.format(ct))
+        
+    for c in range(ncols):
+        
+        if hist:
+            axes[0,c].imshow(img[:,:,c])
+            axes[0,c].set_title('Bands {}'.format(c))
                 
-                axes[r,2*c+1].hist(img[:,:,ct].flatten(), 200)
-                axes[r,2*c+1].set_title('Histogram of Band {}'.format(ct))
-            else:
-                axes[r,c].imshow(img[:,:,ct])
-                axes[r,c].set_title('Band {}'.format(ct))
-            ct+=1
+            axes[1,c].hist(img[:,:,c].flatten(), 200)
+            axes[1,c].set_title('Histogram of Band {}'.format(c))
+        else:
+            axes[c].imshow(img[:,:,c])
+            axes[c].set_title('Band {}'.format(c))
 
     fig.tight_layout()
     plt.show()
