f43194059dbd6a8641540321565c2f851aa24e90
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2023-08-08 15:31:28 +0200

    better comments

--- pyosv/utils/paths.py
+++ pyosv/utils/paths.py
@@ -2,19 +2,38 @@ from tkinter.filedialog import askopenfilename
 from tkinter import Tk
 
 
-def get_path_gui():
+def get_path_gui() -> str:
     
     '''
         Get the path of a file using a graphical menu    
         
-        Output:
+        Parameters:
+        -----------
+        Nothing
+
+        Returns:
+        --------
             - path: path of the image
+
+        Usage:
+        ------
+
+        ```python
+        path = get_path_gui()
+        ```
+
+        Output:
+        -------
+        ```
+        /path/to/file/img.png
+        ```
+
     '''
     
     root = Tk()
     root.withdraw()
     path = askopenfilename(
-        title='Select the image to read!',
+        title='Select the file or the folder!',
         initialdir='/'
         )
     root.destroy()
